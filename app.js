74536cbe4bf0c2436597a793aa633d682546bec7
Michelle <michelle.douma@gmail.com>
2022-07-04 16:25:57 -0700

    added name prompt feature

--- app.js
+++ app.js
@@ -13,10 +13,19 @@ const inquirer = require('inquirer');
 
 const promptUser = () => {
     return inquirer.prompt([
+     
       {
         type: 'input',
         name: 'name',
-        message: 'What is your name?'
+        message: 'What is your name? (Required)',
+        validate: nameInput => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log('Please enter your name!');
+            return false;
+          }
+        }
       },
       {
         type: 'input',
